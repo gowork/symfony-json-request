b800b8af653a6aae5f9de67ad96e594f21a308c1
bronislawbialek <after89@gmail.com>
2024-11-11 21:21:37 +0100

    Add support for Symfony 7
    - update min PHP ver. to 8.1
    - remove Symfony 4.x from composer.json
    - remove Psalm
    - add info about getPayload

--- src/JsonBodyToRequestListener.php
+++ src/JsonBodyToRequestListener.php
@@ -5,6 +5,7 @@ namespace GW\SymfonyJsonRequest;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use function is_array;
 use function is_string;
+use function method_exists;
 use function Safe\json_decode;
 
 final class JsonBodyToRequestListener
@@ -12,8 +13,20 @@ final class JsonBodyToRequestListener
     public function onKernelRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
+        $contentType = null;
 
-        if ($request->getContentType() !== 'json' || !$event->isMainRequest()) {
+        // https://github.com/symfony/symfony/commit/57b24fec9627045e827883d4a501691d2d6976fe
+        // could be dropped after drop support for symfony 6.x
+
+        if (method_exists($request, 'getContentType')) {
+            $contentType = $request->getContentType();
+        }
+
+        if (method_exists($request, 'getContentTypeFormat')) {
+            $contentType = $request->getContentTypeFormat();
+        }
+
+        if ($contentType !== 'json' || !$event->isMainRequest()) {
             return;
         }
 
